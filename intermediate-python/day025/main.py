b7c5e91a9aecf546fa813708eee94ebcd7e5356a
AlexeiLuchian <alexeiluchianx@gmail.com>
2025-08-30 16:43:31 +0300

    Add data saving with .csv

--- intermediate-python/day025/main.py
+++ intermediate-python/day025/main.py
@@ -26,12 +26,21 @@ text.hideturtle()
 text.penup()
 
 while game_on:
-    answer = screen.textinput(title=f"{nr_of_correct_guesses}/50 States Correct", prompt="What's another state?").title()
-    if answer in states_dict and answer not in correct_guesses:
-        text.goto(states_dict[answer])
-        text.write(answer)
-        nr_of_correct_guesses += 1
-        correct_guesses.append(answer)
+    answer = screen.textinput(title=f"{nr_of_correct_guesses}/50 States Correct", prompt="What's another state?")
+    if answer:
+        answer = answer.title()
+        if answer in states_dict and answer not in correct_guesses:
+            text.goto(states_dict[answer])
+            text.write(answer)
+            nr_of_correct_guesses += 1
+            correct_guesses.append(answer)
     if answer == None or nr_of_correct_guesses == 50:
         game_on = False
     
+states_to_learn = []
+for state in states_dict:
+    if state not in correct_guesses:
+        states_to_learn.append(state)
+
+new_dataframe = pandas.DataFrame(states_to_learn)
+new_dataframe.to_csv('states_to_learn.csv')
