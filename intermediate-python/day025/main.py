7e6876d242986b169e413a12ea20998120fc0945
AlexeiLuchian <alexeiluchianx@gmail.com>
2025-08-30 16:29:22 +0300

    Finish game functionality

--- intermediate-python/day025/main.py
+++ intermediate-python/day025/main.py
@@ -1,4 +1,4 @@
-import turtle
+import turtle, pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -7,6 +7,31 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer = screen.textinput(title="Guess the state", prompt="What's another state?")
+data = pandas.read_csv("50_states.csv")
 
-print(answer)
+states_list = data["state"].to_list()
+x_list = data["x"].to_list()
+y_list = data["y"].to_list()
+
+states_dict = {}
+for i in range(len(states_list)):
+    states_dict[states_list[i]] = (x_list[i], y_list[i])
+
+correct_guesses = []
+nr_of_correct_guesses = 0
+
+game_on = True
+text = turtle.Turtle()
+text.hideturtle()
+text.penup()
+
+while game_on:
+    answer = screen.textinput(title=f"{nr_of_correct_guesses}/50 States Correct", prompt="What's another state?").title()
+    if answer in states_dict and answer not in correct_guesses:
+        text.goto(states_dict[answer])
+        text.write(answer)
+        nr_of_correct_guesses += 1
+        correct_guesses.append(answer)
+    if answer == None or nr_of_correct_guesses == 50:
+        game_on = False
+    
