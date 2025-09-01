9a3a1e45e0f78138e092b523234fccd87678f8fc
AlexeiLuchian <alexeiluchianx@gmail.com>
2025-09-01 16:50:53 +0300

    Finish day027 task

--- intermediate-python/day027/main.py
+++ intermediate-python/day027/main.py
@@ -1,19 +1,34 @@
 import tkinter
 
-def button_clicked():
-    label["text"] = f"Aaaaah, it was you {user_input.get()}"
+def calculate():
+    number = entry.get()
+    if number.isnumeric():
+        number_label.config(text=round(1.60934*float(number),2))
+    else:
+        number_label.config(text=0.0)
 
 window = tkinter.Tk()
-window.title("My first GUI program")
-window.minsize(width=500, height=300)
+window.title("Miles to Km Converter")
+window.config(padx=50, pady=50)
 
-label = tkinter.Label(text="I don't know you")
-label.pack()
+entry = tkinter.Entry(width=10)
+entry.insert("0","0")
+entry.grid(row=0, column=1)
 
-button = tkinter.Button(text="button", command=button_clicked)
-button.pack()
+miles_label = tkinter.Label(text="Miles")
+miles_label.grid(row=0, column=2)
 
-user_input = tkinter.Entry(width=20)
-user_input.pack()
+is_equal_label = tkinter.Label(text="is equal to")
+is_equal_label.grid(row=1,column=0)
+
+number_label = tkinter.Label(text=0.0)
+number_label.grid(row=1,column=1)
+
+km_label = tkinter.Label(text="Km")
+km_label.grid(row=1,column=2)
+
+button = tkinter.Button(text="Calculate", command=calculate)
+button.grid(row=2,column=1)
+
 
 window.mainloop()
