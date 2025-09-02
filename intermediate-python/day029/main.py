bcb53214f746e15b1514ca41b88a07f51681d6d8
AlexeiLuchian <alexeiluchianx@gmail.com>
2025-09-02 15:14:57 +0300

    Setup UI

---
+++ intermediate-python/day029/main.py
@@ -0,0 +1,41 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.minsize(width=300,height=200)
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+site_label = Label(text="Website:")
+site_label.grid(row=1, column=0)
+
+mail_label = Label(text="Email/Username:")
+mail_label.grid(row=2, column=0)
+
+pass_label = Label(text="Password:")
+pass_label.grid(row=3, column=0)
+
+site_entry = Entry(width=43)
+site_entry.grid(row=1, column=1, columnspan=2)
+
+mail_entry = Entry(width=43)
+mail_entry.grid(row=2, column=1, columnspan=2)
+
+pass_entry = Entry(width=24)
+pass_entry.grid(row=3, column=1)
+
+gen_pass_button = Button(text="Generate Password")
+gen_pass_button.grid(row=3, column=2)
+
+add_pass_button = Button(text="Add", width=40)
+add_pass_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
