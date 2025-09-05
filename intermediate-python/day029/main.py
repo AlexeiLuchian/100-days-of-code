6fca6e7a26ce1d050f5985726a4aaf31a9cc351a
AlexeiLuchian <alexeiluchianx@gmail.com>
2025-09-05 16:52:59 +0300

    Finish day029 task Implement Password Generation functionality by adding and optimizing the 'Password Generator' project.

--- intermediate-python/day029/main.py
+++ intermediate-python/day029/main.py
@@ -1,6 +1,29 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+#Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = [random.choice(letters) for _ in range(nr_letters)]
+    password_list += [random.choice(symbols) for _ in range(nr_symbols)]
+    password_list += [random.choice(numbers) for _ in range(nr_numbers)]
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    pyperclip.copy(password)
+    pass_entry.delete("0", END)
+    pass_entry.insert(END, password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_pass():
@@ -51,7 +74,7 @@ mail_entry.insert(END, "default@mail.com")
 pass_entry = Entry(width=24)
 pass_entry.grid(row=3, column=1)
 
-gen_pass_button = Button(text="Generate Password")
+gen_pass_button = Button(text="Generate Password", command=generate_password)
 gen_pass_button.grid(row=3, column=2)
 
 add_pass_button = Button(text="Add", width=41, command=save_pass)
