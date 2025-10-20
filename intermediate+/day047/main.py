78b0f0013f12b739651b276605315f0e7d719589
AlexeiLuchian <alexeiluchianx@gmail.com>
2025-10-20 19:16:19 +0200

    Send email if price below target

--- intermediate+/day047/main.py
+++ intermediate+/day047/main.py
@@ -1,7 +1,16 @@
 import requests
+import smtplib
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+email = os.environ.get("FROM_EMAIL")
+password = os.environ.get("PASSWORD")
 instant_pot_endpoint = "https://appbrewery.github.io/instant_pot/"
+headers = {
+    "Content-Type": "text/plain; charset=utf-8",
+}
 
 response = requests.get(instant_pot_endpoint)
 response.raise_for_status()
@@ -11,4 +20,13 @@ price_whole = soup.select_one("span.a-price-whole").getText()
 price_fraction = soup.select_one("span.a-price-fraction").getText()
 price = float(price_whole + price_fraction)
 
-print(price)
+if price < 100:
+    product = " ".join(soup.find(name="span", id="productTitle").getText().split()).encode('utf-8')
+    link = soup.find(name="a", id="bylineInfo")['href']
+    message = f"{product} is available now for the price of {price}$.\n{link}"
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=email, password=password)
+        connection.sendmail(from_addr=email,
+                            to_addrs=email,
+                            msg=f"Subject:DISCOUNT!\n\n{message}")
