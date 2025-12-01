b114b23fa93de64a6b6db93fedd5edf78481e6fa
AlexeiLuchian <alexeiluchian999@gmail.com>
2025-12-01 22:58:39 +0200

    Automate Cookie Clicker Game

--- intermediate+/day048/main.py
+++ intermediate+/day048/main.py
@@ -1,16 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
+driver.implicitly_wait(5)
 
-driver.get("https://www.python.org/")
+driver.get("https://ozh.github.io/cookieclicker/")
 
-elements_name = driver.find_elements(By.CSS_SELECTOR, "div.medium-widget.event-widget.last > div > ul > li > a")
-elements_time = driver.find_elements(By.CSS_SELECTOR, "div.medium-widget.event-widget.last > div > ul > li > time")
+language_btn = driver.find_element(By.CSS_SELECTOR, 'div.langSelectButton')
+language_btn.click()
 
-events_dict = {i:{'name': elements_name[i].text, 'time' : elements_time[i].text} for i in range(len(elements_name))}
-print(events_dict)
+cookie_btn = driver.find_element(By.ID, "bigCookie")
+products_btns = driver.find_elements(By.CSS_SELECTOR, 'div.product')
+
+game_time = time.time() + 5 * 60
+end_time = time.time() + 5
+while time.time() < game_time:
+    cookie_btn.click()
+    if time.time() > end_time:
+        for i in range(len(products_btns) -1, -1, -1):
+            if products_btns[i].is_displayed() and products_btns[i].is_enabled():
+                products_btns[i].click()
+        end_time = time.time() + 5
+
+cps_box = driver.find_element(By.ID, 'cookiesPerSecond')
+cps_box_text = cps_box.text
 
 driver.quit()
+print(cps_box_text)
